ca0c5e13a767445efbc3126e3992a25df1aa7049
Timoxa1329 <timafe2012@yandex.ru>
2020-12-10 12:01:05 +0300

    Работает!!!

--- Kalkulator/Form1.Designer.cs
+++ Kalkulator/Form1.Designer.cs
@@ -36,17 +36,13 @@ namespace Kalkulator
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(14, 68);
-            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Location = new System.Drawing.Point(142, 68);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(90, 33);
             this.button1.TabIndex = 0;
@@ -58,7 +54,7 @@ namespace Kalkulator
             // 
             this.textBox1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox1.Location = new System.Drawing.Point(31, 29);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(62, 29);
             this.textBox1.TabIndex = 1;
@@ -80,7 +76,7 @@ namespace Kalkulator
             // 
             this.textBox2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox2.Location = new System.Drawing.Point(160, 29);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(5);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(54, 29);
             this.textBox2.TabIndex = 3;
@@ -89,7 +85,7 @@ namespace Kalkulator
             // 
             this.textBox3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.textBox3.Location = new System.Drawing.Point(264, 29);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(5);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(51, 29);
             this.textBox3.TabIndex = 4;
@@ -116,56 +112,11 @@ namespace Kalkulator
             this.label3.TabIndex = 6;
             this.label3.Text = "=0";
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(161, 101);
-            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 21);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "x2 =";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(161, 68);
-            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 21);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "x1 =";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox4.Location = new System.Drawing.Point(215, 65);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(5);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(51, 29);
-            this.textBox4.TabIndex = 10;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox5.Location = new System.Drawing.Point(215, 98);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(5);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(51, 29);
-            this.textBox5.TabIndex = 11;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 150);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(375, 117);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox3);
@@ -174,7 +125,7 @@ namespace Kalkulator
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "Form1";
             this.Text = "Реш ^2 Ур";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -192,10 +143,6 @@ namespace Kalkulator
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- Kalkulator/Form1.cs
+++ Kalkulator/Form1.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-#include <math.h>
 
 namespace Kalkulator
 {
@@ -36,29 +35,38 @@ namespace Kalkulator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a = Convert.ToDouble(textBox1.Text);
-            double b = Convert.ToDouble(textBox2.Text);
-            double c = Convert.ToDouble(textBox3.Text);
-            double d;
-            if (a != 0)
+            float a = 0;
+            float b = 0;
+            float c = 0;
+
+            a = float.Parse(textBox1.Text);
+            b = float.Parse(textBox2.Text);
+            c = float.Parse(textBox3.Text);
+            if (a == 0)
             {
-                d = b * b - 4 * a * c;
-                if (sqrt(d) > 0) ;
+                float x = -c / b;
+
+                MessageBox.Show("Линейное уравнение, x=" + x);
             }
-            if (d < 0)
+            else
             {
-                MessageBox.Show("Дискриминант меньше нуля. Корней нет.");
-            }
-            if (d == 0)
-            {
-                double x = (-b / (2 * a));
-                MessageBox.Show("Дискриминант равен нулю. Корень равен " + x + ".");
-            }
-            if (d > 0)
-            {
-                double x1 = ((-b - Math.Sqrt(d)) / (2 * a));
-                double x2 = ((-b + Math.Sqrt(d)) / (2 * a));
-                MessageBox.Show("Дискриминант равен " + d + ". Первый корень равен " + x1 + ". Второй корень равен " + x2 + ".");
+                float D = b * b - 4 * a * c;
+                if (D < 0)
+                {
+                    MessageBox.Show("Квадратное уравнение, действительных корней нет");
+                }
+                else if (D == 0)
+                {
+                    float x = -b / (2 * a);
+                    MessageBox.Show("Квадратное уравнение, 2 корня совпадают. x1=x2=" + x);
+                }
+                else
+                {
+                    double x1 = System.Math.Sqrt(D);
+                    double x2 = (-b + x1) / (2 * a);
+                    x1 = (-b - x1) / (2 * a);
+                    MessageBox.Show("Квадратное уравнение, x1=" + x1 + ", x2=" + x2);
+                }
             }
         }
 

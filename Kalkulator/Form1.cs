a08480c1290baafa104b5eca9af1a4fbc5aade84
Timoxa1329 <timafe2012@yandex.ru>
2020-12-10 11:44:37 +0300

    Я хз что делать

--- Kalkulator/Form1.cs
+++ Kalkulator/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+#include <math.h>
+
 namespace Kalkulator
 {
     public partial class Form1 : Form
@@ -18,6 +20,54 @@ namespace Kalkulator
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = Convert.ToDouble(textBox3.Text);
+            double d;
+            if (a != 0)
+            {
+                d = b * b - 4 * a * c;
+                if (sqrt(d) > 0) ;
+            }
+            if (d < 0)
+            {
+                MessageBox.Show("Дискриминант меньше нуля. Корней нет.");
+            }
+            if (d == 0)
+            {
+                double x = (-b / (2 * a));
+                MessageBox.Show("Дискриминант равен нулю. Корень равен " + x + ".");
+            }
+            if (d > 0)
+            {
+                double x1 = ((-b - Math.Sqrt(d)) / (2 * a));
+                double x2 = ((-b + Math.Sqrt(d)) / (2 * a));
+                MessageBox.Show("Дискриминант равен " + d + ". Первый корень равен " + x1 + ". Второй корень равен " + x2 + ".");
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
         {
 
         }
